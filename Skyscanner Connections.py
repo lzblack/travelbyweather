55a122f47c6dcb9e9f0e2de359112f6624ae9880
Zhi Li <lzblack@users.noreply.github.com>
2019-04-02 10:28:29 -0400

    Update Skyscanner Connections.py

--- Skyscanner Connections.py
+++ Skyscanner Connections.py
@@ -18,26 +18,76 @@ response = requests.get("https://skyscanner-skyscanner-flight-search-v1.p.rapida
     "X-RapidAPI-Key": "85c2cae2d1msh4f45b5e5f5f7ffep19de73jsnf513195c7a59"})
 response_text = response.text
 response_data = json.loads(response_text)
+
 routes = response_data['Routes']
+quotes = response_data['Quotes']
+places = response_data['Places']
+carriers = response_data['Carriers']
+currencies = response_data['Currencies']
+# pprint.pprint(quotes)
+
 routes_with_lowest_price = []
 for route in routes:
     if 'QuoteIds' in route:
         routes_with_lowest_price.append(route)
 cheapest_sort = sorted(routes_with_lowest_price, key=lambda i: i['Price'])
-pprint.pprint(cheapest_sort)
-InputPlaceName = input('Enter Place Name')
+# pprint.pprint(cheapest_sort)
 
-place_name = [response_data['Places']]
+# pprint.pprint(cheapest_sort[0])
 
-#PLACES LIST API
-places = requests.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query={}".format(InputPlaceName),
-  headers={
-    "X-RapidAPI-Key": "85c2cae2d1msh4f45b5e5f5f7ffep19de73jsnf513195c7a59"
-  }
-)
-response_text2 = places.text
-response_data2 = json.loads(response_text2)
-pprint.pprint(response_data2)
+
+
+
+# Example: if you just want the destination info of the cheapest quote
+
+cheapest_routeids = cheapest_sort[0]['QuoteIds'] # this is a list of QuoteIds, the first one is not necessarily the cheapest
+
+cheapest_price = cheapest_sort[0]['Price']
+
+
+
+
+cheapest_route_info = {}
+for q in quotes:
+    if q['QuoteId'] in cheapest_routeids and q['MinPrice'] == cheapest_price:
+        cheapest_route_info = q
+
+# pprint.pprint(cheapest_route_info)
+
+
+cheapest_route_dest_info = {}
+for p in places:
+    if cheapest_route_info['OutboundLeg']['DestinationId'] == p['PlaceId']:
+        cheapest_route_dest_info = p
+
+pprint.pprint(cheapest_route_dest_info)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# InputPlaceName = input('Enter Place Name')
+#
+# place_name = [response_data['Places']]
+
+# #PLACES LIST API
+# places = requests.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query={}".format(InputPlaceName),
+#   headers={
+#     "X-RapidAPI-Key": "85c2cae2d1msh4f45b5e5f5f7ffep19de73jsnf513195c7a59"
+#   }
+# )
+# response_text2 = places.text
+# response_data2 = json.loads(response_text2)
+# pprint.pprint(response_data2)
 
 
 
